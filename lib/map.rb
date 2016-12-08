48f7189732a660558893ddf5ac70954b8c961426
schepens83 <schepens@gmail.com>
2016-12-08 14:15:25 +0100

    add checks on correct input file

--- lib/map.rb
+++ lib/map.rb
@@ -1,6 +1,56 @@
 # Representation of the map that needs to be traversed.
 class Map
-	def add(*args)
-		return 0
+	attr_reader :grid
+
+	def initialize()
+		@grid = Hash.new
+	end
+
+	def read_file(path)
+		n = 1
+		m = 1
+		if correct_file?(path)
+			
+			File.open(path, "r") do |f|
+				f.each_line do |line|
+					line.split(" ").map { |s| s.to_i }.each do |val|
+						@grid[[n,m]] = val
+						m = m + 1
+					end
+					n = n + 1
+					m = 1
+				end
+			end
+
+			return true
+		else
+			return false
+		end
+	end
+
+	private
+
+	def correct_file?(path)
+		correct = true
+
+		File.open(path, "r") do |f|
+			f.each_line do |line|
+				unless spaces_between_values?(line) && only_numbers?(line)
+					correct = false
+				end
+			end
+		end
+
+		return correct
+	end
+
+	def only_numbers?(str)
+		str.split(" ").all? { |val| val !~ /\D/ }
+	end
+
+	def spaces_between_values?(str)
+		str.split(" ").length > 1
 	end
 end
+
+# "./Colorado_480x480.dat"
--- spec/map_spec.rb
+++ spec/map_spec.rb
@@ -1,11 +1,27 @@
 require "map"
 
 describe Map do
-	describe ".add" do
-    context "given an empty string" do
-      it "returns zero" do
-        expect(Map.new.add("")).to eql(0)
+	describe ".read_file" do
+		context "given correct file" do
+			it "returns true" do
+				map = Map.new
+				expect(map.read_file("./spec/map_spec_data")).to be true
       end
+
+			it "returns the correct value for a grid location" do
+				map = Map.new
+				map.read_file("./spec/map_spec_data")
+				expect(map.grid[[1,1]]).to eql(1)
+      end
+
     end
+
+		context "given incorrect file" do
+			it "returns false" do
+				map = Map.new
+				expect(map.read_file("./spec/map_spec_data_incorrect")).to be false
+      end
+
+		end
   end
 end
