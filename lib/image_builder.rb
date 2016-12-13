6a67b7b9c745161db774984eca604e60173a6d3b
schepens83 <schepens@gmail.com>
2016-12-13 14:37:52 +0100

    fix the block function draw_route_per_each_pixel

--- lib/image_builder.rb
+++ lib/image_builder.rb
@@ -6,10 +6,9 @@ class ImageBuilder
 	def initialize(map)
 		@map = map
 		@image = ChunkyPNG::Image.new(@map.nr_columns, @map.nr_rows, ChunkyPNG::Color::TRANSPARENT)
-
 	end
 
-
+	# draw the map on @image.
 	def draw_map
 		@map.nr_rows.times do |row|
 			@map.nr_columns.times do |col|
@@ -21,28 +20,25 @@ class ImageBuilder
 		end
 	end
 
-	def draw_route(route)
-		if block_given?  
-			
-			route.each do |e|  
-				col = e[1]
-				row = e[0]
-				@image[ col - 1,row - 1 ] = ChunkyPNG::Color.rgba(255, 0, 0, 128)
-				
-				yield
-			end
-			
-		else  
-
-			route.each do |e|  
-				col = e[1]
-				row = e[0]
-				@image[ col - 1,row - 1 ] = ChunkyPNG::Color.rgba(255, 0, 0, 128)
-			end
-
-		end  
+	# yield after each pixel is drawn. e.g. to save the image
+	def draw_route_per_each_pixel(route)
+		route.length.times do
+			pixel = route.shift
+			draw_route([pixel])
+			yield
+		end
 	end
 
+	# draw the route on @image.
+	def draw_route(route)
+		route.each do |e|  
+			col = e[1]
+			row = e[0]
+			@image[ col-1 , row-1 ] = ChunkyPNG::Color.rgba(255, 0, 0, 128)
+		end
+	end
+
+	# save the image to filename location.
 	def save_image(filename)
 		@image.save(filename, :interlace => true)		
 	end
@@ -54,6 +50,5 @@ class ImageBuilder
 	def normalize(min, max, val)
 		(val - min) / (max - min)
 	end
-	
 
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,7 @@
 require_relative "map"
 require_relative "image_builder"
 require_relative "route_picker"
+# require 'streamio-ffmpeg'
 
 map = Map.new
 map.read_file("./data/Colorado_480x480.dat")
@@ -14,10 +15,25 @@ route = Array.new
 
 	ib = ImageBuilder.new(map)
 	ib.draw_map
-	e = 1
-	ib.draw_route(route) do 
-		ib.save_image("img/Colorado_#{e.to_s.rjust(4, "0")}.png")
-		e = e + 1
+
+	i = 1
+	ib.draw_route_per_each_pixel(route) do 
+		ib.save_image("img/Colorado_#{i.to_s.rjust(4, "0")}.png")
+		i += 1
+		print "#{i.to_s.rjust(4, "0")}\r"
 	end
 
+	# ib.draw_route(route) 
+	# ib.save_image("img/Colorado_#{1.to_s.rjust(4, "0")}.png")
+
 # end
+
+# slideshow_transcoder = FFMPEG::Transcoder.new(
+#   '',
+#   'slideshow.mp4',
+#   { resolution: "480x480" },
+#   input: 'img/Colorado_%03d.jpeg',
+#   input_options: { framerate: '1/5' }
+# )
+
+# slideshow = slideshow_transcoder.run
